e3fa71b68cd0ee149e77ded493261b4b99e7e59b
Christian Pereira da SIlva <59482797+Chrisspe82@users.noreply.github.com>
2022-02-22 22:55:21 -0300

    Criando Domain com cliente, adicionando  referencia ao entityFamework e mapeando domain como dbset

---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="O Campo {} é obrigatorio")]
+        [MaxLength (50, ErrorMessage ="O campo {0} deve ter no maximo {1} Caracteres")]
+        public  string  Nome { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+    }
+}
